c75b23a97f134e8993a3a39057149d5602d544f6
Ulyses28 <pablogutierrezmordini@gmail.com>
2024-11-23 18:56:44 +0100

    model 2

--- AssignmentV2/Main Code/Class.py
+++ AssignmentV2/Main Code/Class.py
@@ -202,9 +202,8 @@ class InvestmentModel1():
     def _build_objective(self):
         revenues = ((self.var.p_N @ self.D.Gen_N_Z.T) * self.DA_Price).sum()  # don't use quicksum here because it's a <MLinExpr (3600, N_zone)>
         op_costs = gp.quicksum(self.var.p_N @ self.D.Gen_N_OpCost)
-        budget_init = self.P.B
         invest_costs = self.var.P_N.T @ self.D.Gen_N_InvCost
-        objective = self.P.R*(revenues - op_costs) + budget_init - invest_costs
+        objective = self.P.R*(revenues - op_costs) - invest_costs
         self.m.setObjective(objective, GRB.MAXIMIZE)
 
 
@@ -253,10 +252,19 @@ class Model_2():
         self.var.d = self.m.addMVar((self.P.N, self.P.N_dem), lb=0)  # demand per hour for every load
         self.var.p_E = self.m.addMVar((self.P.N, self.P.N_gen_E), lb=0)  # power output per hour for every existing generator
         self.var.p_N = self.m.addMVar((self.P.N, self.P.N_gen_N), lb=0) # Power output per hour for every new generator
-        self.var.theta = self.m.addMVar((self.P.N,self.P.N_zone), lb= -1000)  # power flow per hour for every transmission line
-        self.var.DA_Price = self.m.addMVar((self.P.N,self.P.N_zone), lb= -1000) # day ahead price per hour for every zone
+        self.var.theta = self.m.addMVar((self.P.N,self.P.N_zone), lb= -GRB.INFINITY)  # power flow per hour for every transmission line
+        # Dual variables
+        self.var.DA_Price = self.m.addMVar((self.P.N, self.P.N_zone), lb=-GRB.INFINITY)  # Day ahead price per hour for every zone
+        self.var.mu_E_up = self.m.addMVar((self.P.N, self.P.N_gen_E), lb=-GRB.INFINITY)  # Dual 1
+        self.var.mu_E_down = self.m.addMVar((self.P.N, self.P.N_gen_E), lb=-GRB.INFINITY)  # Dual 1
+        self.var.mu_N_up = self.m.addMVar((self.P.N, self.P.N_gen_N), lb=-GRB.INFINITY)  # Dual 2
+        self.var.mu_N_down = self.m.addMVar((self.P.N, self.P.N_gen_N), lb=-GRB.INFINITY)  # Dual 2
+        self.var.nu_up = self.m.addMVar((self.P.N, self.P.N_dem), lb=-GRB.INFINITY)  # Dual 3
+        self.var.nu_down = self.m.addMVar((self.P.N, self.P.N_dem), lb=-GRB.INFINITY)  # Dual 3
+        self.var.ome_up = self.m.addMVar((self.P.N, self.P.N_zone), lb=-GRB.INFINITY)  # Dual 4
+        self.var.ome_down = self.m.addMVar((self.P.N, self.P.N_zone), lb=-GRB.INFINITY)  # Dual 4
+        self.var.rho = self.m.addMVar((self.P.N), lb=-GRB.INFINITY)  # Dual 5
         
-
 
     def _build_constraints(self):
         # Capacity investment constraint
@@ -275,37 +283,110 @@ class Model_2():
         # Max demand constraint
         self.con.max_dem = self.m.addConstr(self.var.d <= self.D.Dem, name='Maximum demand')        
 
-        # Balance constraint
-        prod_zone = self.var.p_E @ self.D.Gen_E_Z.T
-        dem_zone = self.var.d @ self.D.Load_Z.T
-        trans_zone = self.P.Sum_over_hours @ self.Inv_Trans_React.T * (self.var.theta - self.var.theta @ self.D.Trans_Z_Connected_To_Z.T)
-        self.con.balance = self.m.addConstr(dem_zone - prod_zone == -trans_zone, name='Power balance') 
-
         # Power flow constraints, one per transmission line
         self.con.power_flow_0 = self.m.addConstr(self.var.theta @ self.P.first_zone == self.P.voltage_angle_0, name='Initial voltage angle')
         self.Inv_Trans_React = 1/self.D.Trans_React
         Delta_theta = self.var.theta @ self.D.Trans_Line_From_Z - self.var.theta @ self.D.Trans_Line_To_Z
         self.con.power_flow = self.m.addConstr(self.P.Sum_over_hours @ self.Inv_Trans_React.T * Delta_theta <= self.P.Sum_over_hours @ self.D.Trans_Cap.T, name='Power flow constraint')
 
+        # Balance constraint
+        prod_zone = self.var.p_E @ self.D.Gen_E_Z.T
+        dem_zone = self.var.d @ self.D.Load_Z.T
+        trans_zone = self.P.Sum_over_hours @ self.Inv_Trans_React.T * (self.var.theta - self.var.theta @ self.D.Trans_Z_Connected_To_Z.T)
+        self.con.balance = self.m.addConstr(dem_zone - prod_zone == -trans_zone, name='Power balance') 
+
         ## FIRST Order Conditions
 
+        self.con.L_p_EC = (self.m.addConstr(self.D.Gen_E_OpCost - self.var.DA_Price[h] * self.D.Gen_E_Z - self.var.mu_E_up[h] + self.var.mu_E_down[h] == 0, name='L_p_EC') for h in range(self.P.N))
+        self.con.L_p_NC = (self.m.addConstr(self.D.Gen_N_OpCost - self.var.DA_Price[h] * self.D.Gen_N_Z - self.var.mu_N_up[h] + self.var.mu_N_down[h] == 0, name='L_p_NC') for h in range(self.P.N))
+        self.con.L_d = (self.m.addConstr(- self.D.Uti - self.var.DA_Price[h] * self.D.Load_Z - self.var.nu_up[h] + self.var.nu_down[h] == 0, name='L_d') for h in range(self.P.N))
+        self.con.L_O = (self.m.addConstr(self.Inv_Trans_React*(self.var.ome_up[h] - self.var.ome_down[h]) @ self.D.Trans_Line_From_Z[0] 
+                                - self.Inv_Trans_React*(self.var.ome_up[h] - self.var.ome_down[h]) @ self.D.Trans_Line_To_Z[0]
+                                - self.Inv_Trans_React*self.var.DA_Price[h] * self.D.Trans_Line_From_Z[0]
+                                + self.Inv_Trans_React*self.var.DA_Price[h] * self.D.Trans_Line_To_Z[0]
+                                + self.var.rho[h]
+                                == 0, name='L_O') for h in range(self.P.N) for z in range(self.P.N_zone))
+
+        ## Complementary Conditions
+
+        # Auxiliary variables for SOS1
+        aux1 = self.m.addMVar((self.P.N, self.P.N_zone),lb=-gp.GRB.INFINITY)
+        aux2 = self.m.addMVar((self.P.N, self.P.N_zone),lb=-gp.GRB.INFINITY)
+        aux1 == - self.P.Sum_over_hours @ self.D.Trans_Cap.T - self.P.Sum_over_hours @ self.Inv_Trans_React.T * Delta_theta
+        aux2 == self.P.Sum_over_hours @ self.Inv_Trans_React.T * Delta_theta - self.P.Sum_over_hours @ self.D.Trans_Cap.T
+
+        for h in range(self.P.N):
+
+            # Equation 3.29: μEC_g,t · pEC_g,t = 0, μEC_g,t · (P_EC_g − pEC_g,t) = 0
+            for g in range(self.P.N_gen_E):
+
+                self.m.addSOS(1, [self.var.mu_E_down[h,g], self.var.p_E[h,g]])  
+
+                aux = self.m.addVar(lb=-gp.GRB.INFINITY)
+
+                aux == self.D.Gen_E_OpCap[h,g] * self.D.Gen_E_Cap[g] - self.var.p_E[h,g]
+
+                self.m.addSOS(1, [self.var.mu_E_up[h,g], aux])
+
+
+            # Equation 3.30: μNC_g,t · pNC_g,t = 0, μNC_g,t · (P_NC_g − pNC_g,t) = 0
+            for g in range(self.P.N_gen_N):
+
+                self.m.addSOS(1, [self.var.mu_N_down[h,g], self.var.p_N[h,g]])  
+
+                aux = self.m.addVar(lb=-gp.GRB.INFINITY)
+
+                aux == self.D.Gen_N_OpCap[h,g] * self.var.P_N[g] - self.var.p_N[h,g]
+
+                self.m.addSOS(1, [self.var.mu_N_up[h,g], aux])
+
+            # Equation 3.31: νd,t · dd,t = 0, νd,t · (DC_d − dd,t) = 0
+            for d in range(self.P.N_dem):
+
+                self.m.addSOS(1, [self.var.nu_down[h,d], self.var.d[h,d]])  
+
+                aux = self.m.addVar(lb=-gp.GRB.INFINITY)
+
+                aux == self.D.Dem[h,d] - self.var.d[h,d]
+
+                self.m.addSOS(1, [self.var.nu_up[h,d], aux])
+
+            # Equation 3.32: ωz,m,t · linear terms for power flow
+
+            for z in range(self.P.N_zone):  
+
+                self.m.addSOS(1, [self.var.ome_down[h,z], aux1[h,z]])
+
+                self.m.addSOS(1, [self.var.ome_up[h,z], aux2[h,z]])
+
+            # Equation 3.33: ρt · θ1,t = 0
+            self.m.addSOS(1, [self.var.rho[h]  , self.var.theta[h,0]], [1, 1])
 
 
     def _build_objective(self):
-        revenues = ((self.var.p_N @ self.D.Gen_N_Z.T) * self.var.DA_Price).sum()  # don't use quicksum here because it's a <MLinExpr (3600, N_zone)>
+        el_1 = gp.quicksum(self.var.p_E @ self.D.Gen_E_OpCost)
+        el_2 = gp.quicksum(self.D.Gen_E_OpCap[h,g]* self.D.Gen_E_Cap[g] * self.var.mu_E_up[h,g] for h in range(self.P.N) for g in range(self.P.N_gen_E))
+        el_3 = gp.quicksum(self.var.d @ self.D.Uti)
+        el_4 = gp.quicksum(self.D.Dem[h,d] * self.var.nu_up  for h in range(self.P.N) for d in range(self.P.N_dem))
+        el_5 = gp.quicksum((self.P.Sum_over_hours @ self.D.Trans_Cap.T) @ self.var.ome_down)
+        el_6 = gp.quicksum((self.P.Sum_over_hours @ self.D.Trans_Cap.T) @ self.var.ome_up)
         op_costs = gp.quicksum(self.var.p_N @ self.D.Gen_N_OpCost)
-        budget_init = self.P.B
         invest_costs = self.var.P_N.T @ self.D.Gen_N_InvCost
-        objective = self.P.R*(revenues - op_costs) + budget_init - invest_costs
+        objective = self.P.R*(- el_1 - el_2 + el_3 - el_4 - el_5 + el_6 - op_costs) - invest_costs
         self.m.setObjective(objective, GRB.MAXIMIZE)
 
 
     def _display_guropby_results(self):
         self.m.setParam('OutputFlag', self.Guroby_results)
+        self.m.setParam('OutputFlag', self.Guroby_results)
+        self.m.setParam('Method', 2)  # Use barrier method
+        self.m.setParam('Crossover', 0)  # Skip crossover for speed
+        self.m.setParam('Heuristics', 0.2)  # Enable aggressive heuristics
+        self.m.setParam('MIPFocus', 1)  # Focus on finding feasible solutions
     
 
     def _build_model(self):
-        self.m = gp.Model('Investment problem')
+        self.m = gp.Model('Model 2')
         self._build_variables()  
         self._build_constraints()
         self._build_objective()
