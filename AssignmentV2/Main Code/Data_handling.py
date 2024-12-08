d7a95b7fb346cce9e0664979c76299402046f200
bioapfelsaft123 <konstantin.duerr@outlook.de>
2024-12-08 14:55:59 +0100

    Updated CVaR

--- AssignmentV2/Main Code/Data_handling.py
+++ AssignmentV2/Main Code/Data_handling.py
@@ -53,7 +53,7 @@ Trans_React = Trans_React.reshape((Trans_React.shape[0], 1))
 Trans_Cap = Trans_Cap.reshape((Trans_Cap.shape[0], 1))
 
 # Create random scenarios for the investment cost of new generators
-N_S = 3 # Number of scenarios for training 
+N_S = 10 # Number of scenarios for training 
 N_S_test = 20 # Number of scenarios for testing
 max_deviation = 0.5 # 50% of maximum/ min deviation
 Gen_N_Data_scenarios = np.zeros((len(Gen_N_Data),N_S))
