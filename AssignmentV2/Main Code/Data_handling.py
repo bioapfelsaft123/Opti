b80638c20eb59854a4d2ca4838329ee85f39d41b
bioapfelsaft123 <konstantin.duerr@outlook.de>
2024-12-09 13:38:14 +0100

    Updated deterministic problem

--- AssignmentV2/Main Code/Data_handling.py
+++ AssignmentV2/Main Code/Data_handling.py
@@ -55,7 +55,7 @@ Trans_Cap = Trans_Cap.reshape((Trans_Cap.shape[0], 1))
 # Create random scenarios for the investment cost of new generators
 N_S = 5 # Number of scenarios for main models
 N_S_train = 80 # Number of scenarios for training
-N_S_test = 80 # Number of scenarios for testing
+N_S_test = 20 # Number of scenarios for testing
 max_deviation = 0.5 # 50% of maximum/ min deviation
 Gen_N_Data_scenarios = np.zeros((len(Gen_N_Data),N_S))
 Gen_N_OpCost_scenarios = np.zeros((len(Gen_N_OpCost),N_S))
